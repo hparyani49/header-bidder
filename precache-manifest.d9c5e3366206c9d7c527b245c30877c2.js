90eb304bfa3a2ff53d6668bd91d72b0f5aec72d4
Hitesh Paryani <hitesh.paryani@justdial.com>
2020-08-04 23:04:33 +0530

    Updates

---
+++ precache-manifest.d9c5e3366206c9d7c527b245c30877c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb8553f4c20845a82f696f1c8df8149",
+    "url": "/header-bidder/index.html"
+  },
+  {
+    "revision": "f92f258f51b8e93a9da6",
+    "url": "/header-bidder/static/css/main.ba68c624.chunk.css"
+  },
+  {
+    "revision": "129bd3924b1c894e83b4",
+    "url": "/header-bidder/static/js/2.dbc814a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/header-bidder/static/js/2.dbc814a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f92f258f51b8e93a9da6",
+    "url": "/header-bidder/static/js/main.deea2d63.chunk.js"
+  },
+  {
+    "revision": "b8aa114a38f45f9282ec",
+    "url": "/header-bidder/static/js/runtime-main.80d42f3d.js"
+  }
+]);
